1bf331a904ec918d30434a785961793204643000
Sailesh Acharya <sailesh.acharya@usu.edu>
2023-03-19 11:00:12 -0600

    Rename the main script to MNL.

--- scripts/mnl_1.py
+++ scripts/mnl_1.py
@@ -126,70 +126,7 @@ biogeme.calculateNullLoglikelihood(avail=av)
 print(biogeme.estimate().getEstimatedParameters())
 
 # move outpts to outputs folder
-shutil.move("mnl.pickle", "../outputs/mnl.pickle")
-shutil.move("mnl.html", "../outputs/mnl.html")
-
-# ---------------------------------------------------------------------------------------------------------------------#
-## mmnl
-
-# parameters to be estimated
-# alternative specific constants
-# asc_hv = Beta('asc_hv', 0, None, None, 0)
-asc_av = Beta("asc_av", 0, None, None, 0)
-asc_avwl = Beta("asc_avwl", 0, None, None, 0)
-
-# travel time and cost parameters
-b_tt_hv = Beta("b_tt_hv", 0, None, None, 0)
-b_tc_hv = Beta("b_tc_hv", 0, None, None, 0)
-sigma_tt_hv = Beta("sigma_tt_hv", 0, None, None, 0)
-sigma_tc_hv = Beta("sigma_tc_hv", 0, None, None, 0)
-b_tt_hv_rnd = b_tt_hv + sigma_tt_hv * bioDraws("b_tt_hv_rnd", "NORMAL")
-b_tc_hv_rnd = b_tc_hv + sigma_tc_hv * bioDraws("b_tc_hv_rnd", "NORMAL")
-
-b_tt_av = Beta("b_tt_av", 0, None, None, 0)
-b_tc_av = Beta("b_tc_av", 0, None, None, 0)
-sigma_tt_av = Beta("sigma_tt_av", 0, None, None, 0)
-sigma_tc_av = Beta("sigma_tc_av", 0, None, None, 0)
-b_tt_av_rnd = b_tt_av + sigma_tt_av * bioDraws("b_tt_av_rnd", "NORMAL")
-b_tc_av_rnd = b_tc_av + sigma_tc_av * bioDraws("b_tc_av_rnd", "NORMAL")
-
-b_tt_avwl = Beta("b_tt_avwl", 0, None, None, 0)
-b_tc_avwl = Beta("b_tc_avwl", 0, None, None, 0)
-sigma_tt_avwl = Beta("sigma_tt_avwl", 0, None, None, 0)
-sigma_tc_avwl = Beta("sigma_tc_avwl", 0, None, None, 0)
-b_tt_avwl_rnd = b_tt_avwl + sigma_tt_avwl * bioDraws("b_tt_avwl_rnd", "NORMAL")
-b_tc_avwl_rnd = b_tc_avwl + sigma_tc_avwl * bioDraws("b_tc_avwl_rnd", "NORMAL")
-
-# define utility functions
-v1 = b_tt_hv_rnd * hv_tt + b_tc_hv_rnd * hv_tc
-v2 = asc_av + b_tt_av_rnd * av_tt + b_tc_av_rnd * av_tc
-v3 = asc_avwl + b_tt_avwl_rnd * avwl_tt + b_tc_avwl_rnd * avwl_tc
-
-
-# link utility functions with the numbering of alternatives
-v = {1: v1, 2: v2, 3: v3}
-
-# availability of each alternatives
-av = {1: hv_av, 2: av_av, 3: avwl_av}
-
-# define the model
-obsprob = models.logit(v, av, chosen)
-
-# panel data
-database.panel("id")
-condprobIndiv = PanelLikelihoodTrajectory(obsprob)
-logprob = log(MonteCarlo(condprobIndiv))
-
-# estimate the model
-biogeme = bio.BIOGEME(database, logprob, suggestScales=False)
-# biogeme.loadSavedIteration()
-biogeme.modelName = "mmnl"
-
-# get the results in a pandas table
-print(biogeme.estimate().getEstimatedParameters())
-
-# move outpts to outputs folder
-shutil.move("mnnl.pickle", "../outputs/mnnl.pickle")
-shutil.move("mnnl.html", "../outputs/mnnl.html")
+shutil.move("mnl.pickle", "../outputs/mnl_1.pickle")
+shutil.move("mnl.html", "../outputs/mnl_1.html")
 
 # ---------------------------------------------------------------------------------------------------------------------#
